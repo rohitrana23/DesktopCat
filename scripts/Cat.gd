b45b8ebb3dc8bde1933cb7f0b2234b7382eabca3
rohitrana23 <rohitr23122005@gmail.com>
2025-01-31 19:05:55 +0530

    Added a resting chair for cat.

--- scripts/Cat.gd
+++ scripts/Cat.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 225
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -9,6 +9,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var mousePos = Vector2()
 var move
+var goingToRest = false
 
 func _ready():
 	move = false
@@ -16,7 +17,19 @@ func _ready():
 	
 
 func _physics_process(delta):
-	
+	if Input.is_action_just_pressed("q"):
+		get_tree().quit()
+	if  Input.is_action_just_pressed("rmb"):
+		goingToRest = true
+		move = true
+	if goingToRest == true:
+		Vector2.ZERO
+		velocity = position.direction_to(Vector2(1065,530)) * SPEED * 2
+		if position.distance_to(Vector2(1065,530)) < 5:
+			move = false
+			Vector2.ZERO
+			goingToRest = false
+
 	if move == true:
 		$AnimatedSprite2D.play("running")
 	else:
@@ -25,7 +38,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("lmb"):
 		mousePos = get_global_mouse_position()
 		move = true
-	if move == true:
+	if move == true and goingToRest== false:
 		velocity = Vector2.ZERO
 		velocity = position.direction_to(mousePos) * SPEED * 2
 		if position.distance_to(mousePos) < 5:
